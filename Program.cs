30d69769c0cfbfe2235379bf55e09fdbf88e7a61
Yash Thakkar <ythakkar246@gmsil.com>
2025-11-29 18:36:54 +0530

    modified path

--- Program.cs
+++ Program.cs
@@ -5,5 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 app.UseStaticFiles();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Customers}/{action=Index}/{id?}"
+);
 app.MapDefaultControllerRoute();
 app.Run();
